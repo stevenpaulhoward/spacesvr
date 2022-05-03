d3a0288c2809a8545cbd5a93dc402d0d4ac5a310
Alex Shortt <alexander.shortt@gmail.com>
2022-05-02 17:10:18 -0700

    change triple sphere collider to include a cylinder, make thinner

--- src/layers/reality/players/colliders/CapsuleCollider.tsx
+++ src/layers/reality/players/colliders/CapsuleCollider.tsx
@@ -1,17 +1,31 @@
-import { ShapeType, Triplet, useCompoundBody } from "@react-three/cannon";
+import {
+  BodyProps,
+  CylinderArgs,
+  ShapeType,
+  Triplet,
+  useCompoundBody,
+} from "@react-three/cannon";
 import { useEffect, useState } from "react";
 import { useEnvironment } from "../../layers/environment";
+import { CompoundBodyProps } from "@react-three/cannon/src/hooks";
 
 // height of 0.9 (eye level) for a perceived height of 1
 const HEIGHT = 0.9;
-const RADIUS = HEIGHT / 3;
+const RADIUS = 0.2;
+const SEGMENTS = 12;
 
-const SHAPE_TYPE: ShapeType = "Sphere";
+const SPHERE_SHAPE: ShapeType = "Sphere";
+const CYLINDER_SHAPE: ShapeType = "Cylinder";
 
-const sphereProps = { type: SHAPE_TYPE, args: [RADIUS] };
+const sphereProps = { type: SPHERE_SHAPE, args: [RADIUS, SEGMENTS, SEGMENTS] };
+const cylinderProps = {
+  type: CYLINDER_SHAPE,
+  args: [RADIUS, RADIUS, HEIGHT - RADIUS * 2, SEGMENTS],
+};
+
 const sphere1 = { ...sphereProps, position: [0, -(HEIGHT - RADIUS), 0] };
-const sphere2 = { ...sphereProps, position: [0, -(HEIGHT / 2), 0] };
-const sphere3 = { ...sphereProps, position: [0, -RADIUS, 0] };
+const cylinder = { ...cylinderProps, position: [0, -(HEIGHT / 2), 0] };
+const sphere2 = { ...sphereProps, position: [0, -RADIUS, 0] };
 
 export const useCapsuleCollider = (pos = [0, 0, 0]) => {
   const vPos = pos as Triplet;
@@ -22,10 +36,10 @@ export const useCapsuleCollider = (pos = [0, 0, 0]) => {
   const compoundBody = useCompoundBody(() => ({
     mass: 0,
     position: vPos,
-    segments: 8,
+    segments: SEGMENTS,
     fixedRotation: true,
     type: "Dynamic",
-    shapes: [sphere1, sphere2, sphere3],
+    shapes: [sphere1, cylinder, sphere2],
   }));
 
   useEffect(() => {
@@ -41,16 +55,23 @@ export const useCapsuleCollider = (pos = [0, 0, 0]) => {
 export function VisibleCapsuleCollider() {
   const createSphere = (sphere: any) => (
     <mesh position={sphere.position}>
-      <sphereBufferGeometry args={sphere.args} attach="geometry" />
-      <meshStandardMaterial color="red" attach="material" wireframe={true} />
+      <sphereBufferGeometry args={sphere.args} />
+      <meshStandardMaterial color="red" wireframe={true} />
+    </mesh>
+  );
+
+  const createCylinder = (cylinder: any) => (
+    <mesh position={cylinder.position}>
+      <cylinderBufferGeometry args={cylinder.args} />
+      <meshStandardMaterial color="blue" wireframe={true} />
     </mesh>
   );
 
   return (
-    <group name="collider" position={[1.5, 0, 0]}>
+    <group name="collider" position={[1.5, -HEIGHT, 0]}>
       {createSphere(sphere1)}
+      {createCylinder(cylinder)}
       {createSphere(sphere2)}
-      {createSphere(sphere3)}
     </group>
   );
 }
